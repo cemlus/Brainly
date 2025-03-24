1f62c8e917e27f43b672a1a502588f933457035c
siddhant <nerdycoolboi69@gmail.com>
2025-03-24 23:15:39 +0530

    updated models

--- Brainly/src/db/db.ts
+++ Brainly/src/db/db.ts
@@ -3,21 +3,85 @@ import { Schema, Types, model } from "mongoose";
 import mongoose from 'mongoose';
 
 const UserSchema = new Schema({
-    username: {type: String, unique: true, required: true},
-    password: {type: String, required: true},
+    username: { 
+        type: String, 
+        unique: true, 
+        required: true
+    },
+    password: {
+        type: String,
+         required: true
+    },
 })
 
 export const UserModel = model("User", UserSchema);
 
 const contentTypes = ['image', 'video', 'text', 'article'];
 const ContentSchema = new Schema({
-    title: {type: String, required: true},
-    description: {type: String, required: true, default: ''},
-    link: {type: String, required: true},
-    type: {type: String, enum: contentTypes, required: true},
-    tags: [{type: Types.ObjectId, ref: 'Tag'}],
-    userId: [{type: Types.ObjectId, ref: 'User', required: true}],
-    // add more fields like createdAt, updatedAt etc 
+    title: {
+        type: String, 
+        required: true
+    },
+    description: {
+        type: String, 
+        required: true,
+         default: ''
+    },
+    link: {
+        type: String, 
+        required: true
+    },
+    contentType: {
+        type: String, 
+        enum: contentTypes, 
+        required: true
+    },
+    tags: [{
+        type: Types.ObjectId,
+        ref: 'Tag'
+    }],
+    userId: [{
+        type: Types.ObjectId, 
+        ref: 'User', 
+        required: true
+    }],
+    isPublic: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
 })
 
+ContentSchema.pre('save', function(next) {
+    this.updatedAt = new Date(Date.now());
+    next();
+})
 export const ContentModel = model("Content", ContentSchema);
+
+const TagsSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    //
+})
+
+export const TagsModel = model("Tag", TagsSchema);
+
+const linkSchema = new Schema({
+    uniqueHash: {
+        type: String,
+        unique: true,
+        required: true
+    }, 
+    userId: Types.ObjectId
+})
+
+export const LinkModel = model("Link", linkSchema);
